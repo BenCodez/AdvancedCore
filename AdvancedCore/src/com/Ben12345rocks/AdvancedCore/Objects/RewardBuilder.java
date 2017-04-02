cce7cd66e2d56ad1acf5c268d0880c3efe0aebdd
Ben12345rocks <benbergen12@gmail.com>
2017-04-02 13:41:14 -0400

    Add date placeholder, more api

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Objects/RewardBuilder.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Objects/RewardBuilder.java
@@ -1,5 +1,9 @@
 package com.Ben12345rocks.AdvancedCore.Objects;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.bukkit.configuration.file.FileConfiguration;
@@ -15,8 +19,11 @@ public class RewardBuilder {
 	public RewardBuilder(FileConfiguration data, String path) {
 		this.data = data;
 		this.path = path;
-		this.placeholders = new HashMap<String, String>();
 		this.giveOffline = true;
+		this.placeholders = new HashMap<String, String>();
+		LocalDateTime ldt = LocalDateTime.now();
+		Date date = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+		placeholders.put("Date", "" + new SimpleDateFormat("EEE, d MMM yyyy HH:mm").format(date));
 	}
 
 	public RewardBuilder withPrefix(String prefix) {
@@ -41,6 +48,12 @@ public class RewardBuilder {
 
 	public void send(User user) {
 		RewardHandler.getInstance().giveReward(user, prefix, data, path, online, giveOffline, placeholders);
+	}
+
+	public void send(User... users) {
+		for (User user : users) {
+			send(user);
+		}
 	}
 
 	public RewardBuilder setOnline(boolean online) {
