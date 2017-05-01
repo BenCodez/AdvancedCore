29486321bfabe9266fde182190be55f9ced9549b
Ben12345rocks <benbergen12@gmail.com>
2017-05-01 16:53:00 -0400

    Use different craftbukkit handle

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
@@ -3,18 +3,18 @@ package com.Ben12345rocks.AdvancedCore.ServerHandle;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.pl3x.bukkit.chatapi.ComponentSender;
 
 public class CraftBukkitHandle implements IServerHandle {
 
 	@Override
 	public void sendMessage(Player player, BaseComponent component) {
-		//ComponentSender.sendMessage(player, component);
+		ComponentSender.sendMessage(player, component);
 
 	}
 
 	@Override
 	public void sendMessage(Player player, BaseComponent... components) {
-		//ComponentSender.sendMessage(player, components);
+		ComponentSender.sendMessage(player, components);
 	}
-
 }
