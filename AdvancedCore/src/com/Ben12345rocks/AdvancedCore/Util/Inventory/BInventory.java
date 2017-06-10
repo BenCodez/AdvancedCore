348da755ab2809733bb20729da447ec895679c12
Ben12345rocks <benbergen12@gmail.com>
2017-06-10 08:18:51 -0400

    Bug fix

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Inventory/BInventory.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Inventory/BInventory.java
@@ -324,15 +324,15 @@ public class BInventory implements Listener {
 	}
 
 	private int getProperSize(int size) {
-		if (size <= 9) {
+		if (size < 9) {
 			return 9;
-		} else if (size <= 18) {
+		} else if (size < 18) {
 			return 18;
-		} else if (size <= 27) {
+		} else if (size < 27) {
 			return 27;
-		} else if (size <= 36) {
+		} else if (size < 36) {
 			return 36;
-		} else if (size <= 45) {
+		} else if (size < 45) {
 			return 45;
 		} else {
 			return 54;
