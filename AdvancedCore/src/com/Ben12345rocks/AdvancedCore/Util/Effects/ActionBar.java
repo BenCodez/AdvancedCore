e58f5ad64a6ffca296d7068834326bf8a46261a4
Ben12345rocks <benbergen12@gmail.com>
2017-06-11 15:59:32 -0400

    Fix actionbar not working in 1.12

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Effects/ActionBar.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Effects/ActionBar.java
@@ -1,5 +1,6 @@
 package com.Ben12345rocks.AdvancedCore.Util.Effects;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -18,14 +19,13 @@ public class ActionBar {
 
 	AdvancedCoreHook plugin = AdvancedCoreHook.getInstance();
 
-	/** The works. */
-	public boolean works = true;
-
 	/** The nmsver. */
 	public String nmsver;
 
 	/** The use old methods. */
 	private boolean useOldMethods = false;
+
+	private boolean useNewMethods = true;
 
 	/** The duration. */
 	private int duration;
@@ -52,6 +52,10 @@ public class ActionBar {
 			// // for the protocol
 			// // hack?
 			useOldMethods = true;
+		}
+
+		if (nmsver.contains("1.9") || nmsver.contains("1.10") || nmsver.contains("1.11")) {
+			useNewMethods = false;
 		}
 	}
 
@@ -94,33 +98,53 @@ public class ActionBar {
 	 *            the message
 	 */
 	public void sendActionBar(Player player, String message) {
-		try {
-			Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + "entity.CraftPlayer");
-			Object p = c1.cast(player);
-			Object ppoc;
-			Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + "PacketPlayOutChat");
-			Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + "Packet");
-			if (useOldMethods) {
-				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + "ChatSerializer");
-				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + "IChatBaseComponent");
-				Method m3 = c2.getDeclaredMethod("a", String.class);
-				Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-				ppoc = c4.getConstructor(new Class<?>[] { c3, byte.class }).newInstance(cbc, (byte) 2);
-			} else {
-				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + "ChatComponentText");
-				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + "IChatBaseComponent");
-				Object o = c2.getConstructor(new Class<?>[] { String.class }).newInstance(message);
-				ppoc = c4.getConstructor(new Class<?>[] { c3, byte.class }).newInstance(o, (byte) 2);
+		if (!useNewMethods) {
+			try {
+				Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + "entity.CraftPlayer");
+				Object p = c1.cast(player);
+				Object ppoc;
+				Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + "PacketPlayOutChat");
+				Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + "Packet");
+				if (useOldMethods) {
+					Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + "ChatSerializer");
+					Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + "IChatBaseComponent");
+					Method m3 = c2.getDeclaredMethod("a", String.class);
+					Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
+					ppoc = c4.getConstructor(new Class<?>[] { c3, byte.class }).newInstance(cbc, (byte) 2);
+				} else {
+					Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + "ChatComponentText");
+					Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + "IChatBaseComponent");
+					Object o = c2.getConstructor(new Class<?>[] { String.class }).newInstance(message);
+					ppoc = c4.getConstructor(new Class<?>[] { c3, byte.class }).newInstance(o, (byte) 2);
+				}
+				Method m1 = c1.getDeclaredMethod("getHandle");
+				Object h = m1.invoke(p);
+				Field f1 = h.getClass().getDeclaredField("playerConnection");
+				Object pc = f1.get(h);
+				Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
+				m5.invoke(pc, ppoc);
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
-			Method m1 = c1.getDeclaredMethod("getHandle");
-			Object h = m1.invoke(p);
-			Field f1 = h.getClass().getDeclaredField("playerConnection");
-			Object pc = f1.get(h);
-			Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
-			m5.invoke(pc, ppoc);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			works = false;
+		} else {
+			// 1.12+ methods
+			try {
+				Constructor<?> constructor = NMSManager.getInstance().getNMSClass("PacketPlayOutChat").getConstructor(
+						NMSManager.getInstance().getNMSClass("IChatBaseComponent"),
+						NMSManager.getInstance().getNMSClass("ChatMessageType"));
+
+				Object icbc = NMSManager.getInstance().getNMSClass("IChatBaseComponent").getDeclaredClasses()[0]
+						.getMethod("a", String.class).invoke(null, "{\"text\":\"" + msg + "\"}");
+				Object packet = constructor.newInstance(icbc,
+						NMSManager.getInstance().getNMSClass("ChatMessageType").getEnumConstants()[2]);
+				Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
+				Object playerConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
+
+				playerConnection.getClass().getMethod("sendPacket", NMSManager.getInstance().getNMSClass("Packet"))
+						.invoke(playerConnection, packet);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
