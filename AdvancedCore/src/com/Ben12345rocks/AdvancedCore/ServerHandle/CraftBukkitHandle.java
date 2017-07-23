b9b11af43107768927e637c293658a8ddd23e18c
Ben12345rocks <benbergen12@gmail.com>
2017-07-23 16:23:57 -0400

    Bug fix

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
@@ -3,28 +3,18 @@ package com.Ben12345rocks.AdvancedCore.ServerHandle;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.chat.BaseComponent;
-import net.pl3x.bukkit.chatapi.ComponentSender;
 
 public class CraftBukkitHandle implements IServerHandle {
 
 	@Override
 	public void sendMessage(Player player, BaseComponent component) {
-		try {
-			ComponentSender.sendMessage(player, component);
-		} catch (Exception e) {
-			player.sendMessage(component.toPlainText());
-		}
-
+		player.sendMessage(component.toPlainText());
 	}
 
 	@Override
 	public void sendMessage(Player player, BaseComponent... components) {
-		try {
-			ComponentSender.sendMessage(player, components);
-		} catch (Exception e) {
-			for (BaseComponent comp : components) {
-				sendMessage(player, comp);
-			}
+		for (BaseComponent comp : components) {
+			sendMessage(player, comp);
 		}
 	}
 }
