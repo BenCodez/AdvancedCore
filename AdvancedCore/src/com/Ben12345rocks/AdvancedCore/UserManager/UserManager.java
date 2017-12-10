42d3cf8e50aedb24a71e46cc309c2d01ef105613
Ben12345rocks <benbergen12@gmail.com>
2017-12-10 15:24:00 -0500

    Minor improvement

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/UserManager/UserManager.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/UserManager/UserManager.java
@@ -150,6 +150,7 @@ public class UserManager {
 	public boolean userExist(String name) {
 		for (String s : plugin.getUuids().keySet()) {
 			if (s.equalsIgnoreCase(name)) {
+				plugin.extraDebug("Found " + name + " loaded in uuid map");
 				return true;
 			}
 		}
