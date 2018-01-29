c4c1f50274b20df92ac7c5e94a52c7c6f6194b29
Ben12345rocks <benbergen12@gmail.com>
2018-01-29 18:05:24 -0500

    More debug

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/mysql/MySQL.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/mysql/MySQL.java
@@ -234,6 +234,7 @@ public class MySQL {
 	public ArrayList<Column> getExact(String uuid) {
 		// AdvancedCoreHook.getInstance().debug("Get Exact: " + uuid);
 		loadPlayerIfNeeded(uuid);
+		AdvancedCoreHook.getInstance().debug("test one: " + uuid);
 		return table.get(uuid);
 	}
 
