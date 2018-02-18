fc3d37b218e4499a98e361ad562c13128143c814
Ben12345rocks <benbergen12@gmail.com>
2018-02-17 20:35:46 -0500

    Bug fix

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/StringUtils.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/StringUtils.java
@@ -66,6 +66,9 @@ public class StringUtils {
 	}
 
 	public boolean containsIgnorecase(String str1, String str2) {
+		if (str1 == null || str2 == null) {
+			return false;
+		}
 		return str1.toLowerCase().contains(str2.toLowerCase());
 	}
 
