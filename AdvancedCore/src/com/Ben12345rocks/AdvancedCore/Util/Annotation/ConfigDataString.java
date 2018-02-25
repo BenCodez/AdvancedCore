e29f7820ec0e51c93137cca45823d3137b4df437
Ben12345rocks <benbergen12@gmail.com>
2018-02-24 20:36:33 -0500

    More improvements

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Annotation/ConfigDataString.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Annotation/ConfigDataString.java
@@ -2,10 +2,13 @@ package com.Ben12345rocks.AdvancedCore.Util.Annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Inherited
 @Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigDataString {
 	String path();
 	String defaultValue() default "";
