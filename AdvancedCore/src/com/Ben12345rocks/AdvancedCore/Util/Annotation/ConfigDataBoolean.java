3f5c76bf307d119b7d62943dde895da61f9bd104
Ben12345rocks <benbergen12@gmail.com>
2018-02-25 14:52:57 -0500

    More changes

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Annotation/ConfigDataBoolean.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Annotation/ConfigDataBoolean.java
@@ -2,13 +2,10 @@ package com.Ben12345rocks.AdvancedCore.Util.Annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Inherited
 @Target({ ElementType.METHOD, ElementType.FIELD })
-@Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigDataBoolean {
 	String path();
 	boolean defaultValue() default false;
