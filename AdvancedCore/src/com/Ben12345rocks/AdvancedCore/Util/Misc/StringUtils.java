e1184b2ceaf592983ed82312de996568b862f18e
Ben12345rocks <benbergen12@gmail.com>
2018-03-04 16:07:00 -0500

    Bug fix

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/StringUtils.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/StringUtils.java
@@ -231,7 +231,7 @@ public class StringUtils {
 			return replaceIgnoreCase(replaceIgnoreCase(str, "%" + toReplace + "%", replaceWith),
 					"\\{" + toReplace + "\\}", replaceWith);
 		} else {
-			return str.replace(toReplace, replaceWith);
+			return str.replace("%" + toReplace + "%", replaceWith.replace("\\{" + toReplace + "\\}", replaceWith));
 		}
 	}
 
