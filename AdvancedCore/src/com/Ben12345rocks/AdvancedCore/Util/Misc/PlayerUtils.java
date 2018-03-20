486d9500aa1f6337452c37eaf9d91f187c546bfb
Ben12345rocks <benbergen12@gmail.com>
2018-03-19 20:01:21 -0400

    Extra debug

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/PlayerUtils.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Misc/PlayerUtils.java
@@ -323,7 +323,7 @@ public class PlayerUtils {
 			plugin.extraDebug("Checking offline player: " + name);
 			OfflinePlayer p = Bukkit.getOfflinePlayer(name);
 			if (p.hasPlayedBefore()) {
-				// plugin.extraDebug(name + " has joined before");
+				plugin.extraDebug(name + " has joined before");
 				return true;
 			}
 		}
