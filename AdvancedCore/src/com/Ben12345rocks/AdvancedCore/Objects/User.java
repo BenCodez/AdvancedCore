ebe1b13c1cdc5cf3c07caa8c4475bca28fd4dc37
Ben12345rocks <benbergen12@gmail.com>
2018-04-24 20:04:48 -0400

    Add debug

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Objects/User.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Objects/User.java
@@ -860,6 +860,7 @@ public class User {
 		if ((player != null) && (msg != null)) {
 			if (!msg.equals("")) {
 				for (String str : msg.split("%NewLine%")) {
+					AdvancedCoreHook.getInstance().debug(msg);
 					AdvancedCoreHook.getInstance().getServerHandle().sendMessage(player,
 							StringUtils.getInstance().parseJson(StringUtils.getInstance().parseText(player, str)));
 				}
