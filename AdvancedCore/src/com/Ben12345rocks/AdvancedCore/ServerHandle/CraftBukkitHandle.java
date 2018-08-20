24b98e895213b70fea5487b43c883a4132751bec
Ben12345rocks <benbergen12@gmail.com>
2018-08-20 19:57:50 -0400

    Bug fix

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/ServerHandle/CraftBukkitHandle.java
@@ -34,9 +34,9 @@ public class CraftBukkitHandle implements IServerHandle {
 			Object connection = ReflectionUtils.getFieldValue(playerConnection, handle);
 			ReflectionUtils.invokeMethod(sendPacket, connection, packetPlayOutChat);
 		} catch (Exception e) {
-
+			player.sendMessage(component.toPlainText());
 		}
-		player.sendMessage(component.toPlainText());
+
 	}
 
 	@Override
