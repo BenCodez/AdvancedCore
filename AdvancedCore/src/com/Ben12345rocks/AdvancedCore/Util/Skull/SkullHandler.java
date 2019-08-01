843e3775f3210d89c387cea53197cea2a693a370
Ben12345rocks <benbergen12@gmail.com>
2019-07-31 20:45:30 -0400

    Remove not working skull api

--- AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Skull/SkullHandler.java
+++ AdvancedCore/src/com/Ben12345rocks/AdvancedCore/Util/Skull/SkullHandler.java
@@ -1,18 +1,13 @@
 package com.Ben12345rocks.AdvancedCore.Util.Skull;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Base64;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import com.Ben12345rocks.AdvancedCore.AdvancedCorePlugin;
 import com.Ben12345rocks.AdvancedCore.NMSManager.NMSManager;
@@ -59,11 +54,9 @@ public class SkullHandler {
 
 	@SuppressWarnings("deprecation")
 	public ItemStack getSkull(String playerName) {
-		if (playerName.startsWith("url:")) {
-			return getHead(playerName.substring("url:".length()));
-		} else {
-			return new ItemBuilder(Material.PLAYER_HEAD).setSkullOwner(playerName).toItemStack();
-		}
+
+		return new ItemBuilder(Material.PLAYER_HEAD).setSkullOwner(playerName).toItemStack();
+
 	}
 
 	public boolean hasSkull(String playerName) {
@@ -75,49 +68,54 @@ public class SkullHandler {
 		return false;
 	}
 
-	@SuppressWarnings("rawtypes")
-	private Class gameProfile;
-
-	@SuppressWarnings("rawtypes")
-	private Class property;
-	@SuppressWarnings("rawtypes")
-	private Constructor gameProfileConstructor;
-	@SuppressWarnings("rawtypes")
-	private Constructor propertyConstructor;
-	private Method gameProfileGetProperties;
-
-	public ItemStack getHead(String url) {
-		ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
-		ItemMeta headMeta = head.getItemMeta();
-
-		try {
-			// GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-			Object profile = gameProfileConstructor.newInstance(UUID.randomUUID(), null);
-			byte[] encodedData = Base64.getEncoder()
-					.encode((String.format("{\"textures\":{\"SKIN\":{\"url\":\"%s\"}}}", url).getBytes()));
-
-			gameProfileGetProperties.invoke(gameProfile, "textures",
-					propertyConstructor.newInstance("textures", new String(encodedData)));
-			Field profileField = null;
-			profileField = headMeta.getClass().getDeclaredField("profile");
-			profileField.setAccessible(true);
-			profileField.set(headMeta, profile);
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException
-				| InvocationTargetException | InstantiationException e) {
-			e.printStackTrace();
-		}
-		head.setItemMeta(headMeta);
-		return head;
-	}
+	/*
+	 * @SuppressWarnings("rawtypes")
+	 * private Class gameProfile;
+	 * @SuppressWarnings("rawtypes")
+	 * private Class property;
+	 * @SuppressWarnings("rawtypes")
+	 * private Constructor gameProfileConstructor;
+	 * @SuppressWarnings("rawtypes")
+	 * private Constructor propertyConstructor;
+	 * private Method gameProfileGetProperties;
+	 * public ItemStack getHead(String url) {
+	 * ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
+	 * ItemMeta headMeta = head.getItemMeta();
+	 * try {
+	 * // GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+	 * Object profile = gameProfileConstructor.newInstance(UUID.randomUUID(), null);
+	 * byte[] encodedData = Base64.getEncoder()
+	 * .encode((String.format("{\"textures\":{\"SKIN\":{\"url\":\"%s\"}}}",
+	 * url).getBytes()));
+	 * gameProfileGetProperties.invoke(gameProfile, "textures",
+	 * propertyConstructor.newInstance("textures", new String(encodedData)));
+	 * Field profileField = null;
+	 * profileField = headMeta.getClass().getDeclaredField("profile");
+	 * profileField.setAccessible(true);
+	 * profileField.set(headMeta, profile);
+	 * } catch (IllegalArgumentException | IllegalAccessException |
+	 * NoSuchFieldException | SecurityException
+	 * | InvocationTargetException | InstantiationException e) {
+	 * e.printStackTrace();
+	 * }
+	 * head.setItemMeta(headMeta);
+	 * return head;
+	 * }
+	 */
 
 	@SuppressWarnings("unchecked")
 	public void load() {
 		try {
-			gameProfile = ReflectionUtils.getClassForName("com.mojang.authlib.GameProfile");
-			property = ReflectionUtils.getClassForName("com.mojang.authlib.properties.Property");
-			gameProfileConstructor = gameProfile.getConstructor(UUID.class, String.class);
-			propertyConstructor = property.getConstructor(String.class, String.class);
-			gameProfileGetProperties = gameProfile.getDeclaredMethod("getProperties");
+			/*
+			 * gameProfile =
+			 * ReflectionUtils.getClassForName("com.mojang.authlib.GameProfile");
+			 * property =
+			 * ReflectionUtils.getClassForName("com.mojang.authlib.properties.Property");
+			 * gameProfileConstructor = gameProfile.getConstructor(UUID.class,
+			 * String.class);
+			 * propertyConstructor = property.getConstructor(String.class, String.class);
+			 * gameProfileGetProperties = gameProfile.getDeclaredMethod("getProperties");
+			 */
 
 			craftItemStack = ReflectionUtils.getClassForName(
 					"org.bukkit.craftbukkit." + NMSManager.getInstance().getVersion() + "inventory.CraftItemStack");
