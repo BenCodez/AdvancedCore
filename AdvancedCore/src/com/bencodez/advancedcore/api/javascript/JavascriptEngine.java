a44bf797c94e2ab729ee1b5499b605a3746f928f
BenCodez <benbergen12@gmail.com>
2021-06-12 20:46:44 -0400

    Fix javascript API for java 16 & 1.17

--- AdvancedCore/src/com/bencodez/advancedcore/api/javascript/JavascriptEngine.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/javascript/JavascriptEngine.java
@@ -1,5 +1,7 @@
 package com.bencodez.advancedcore.api.javascript;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -17,6 +19,7 @@ import com.bencodez.advancedcore.api.messages.StringParser;
 import com.bencodez.advancedcore.api.rewards.RewardHandler;
 import com.bencodez.advancedcore.api.user.AdvancedCoreUser;
 import com.bencodez.advancedcore.api.user.UserManager;
+import com.bencodez.advancedcore.nms.ReflectionUtils;
 
 public class JavascriptEngine {
 	private HashMap<String, Object> engineAPI;
@@ -121,28 +124,57 @@ public class JavascriptEngine {
 		return false;
 	}
 
+	public ScriptEngine getJSScriptEngine() {
+		if (Double.parseDouble(System.getProperty("java.specification.version")) < 15) {
+			return new ScriptEngineManager().getEngineByName("js");
+		} else {
+			Class<?> factory = ReflectionUtils
+					.getClassForName("org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory");
+			Method methodToUse = null;
+			for (Method m : factory.getDeclaredMethods()) {
+				if (m.getParameterCount() == 0) {
+					if (m.getName().equals("getScriptEngine")) {
+						methodToUse = m;
+					}
+				}
+			}
+			try {
+				return (ScriptEngine) methodToUse.invoke(factory.newInstance(), new Object[] {});
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
 	public Object getResult(String expression) {
 		if (!expression.equals("")) {
-			ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
-			engine.put("Bukkit", Bukkit.getServer());
-			engine.put("AdvancedCore", AdvancedCorePlugin.getInstance());
-			engine.put("Console", Bukkit.getConsoleSender());
-			engine.put("UserManager", UserManager.getInstance());
-			engine.put("RewardHandler", RewardHandler.getInstance());
-			engine.put("StringParser", StringParser.getInstance());
+			ScriptEngine engine = getJSScriptEngine();
+			if (engine != null) {
+				engine.put("Bukkit", Bukkit.getServer());
+				engine.put("AdvancedCore", AdvancedCorePlugin.getInstance());
+				engine.put("Console", Bukkit.getConsoleSender());
+				engine.put("UserManager", UserManager.getInstance());
+				engine.put("RewardHandler", RewardHandler.getInstance());
+				engine.put("StringParser", StringParser.getInstance());
 
-			engineAPI.putAll(AdvancedCorePlugin.getInstance().getJavascriptEngine());
+				engineAPI.putAll(AdvancedCorePlugin.getInstance().getJavascriptEngine());
 
-			for (Entry<String, Object> entry : engineAPI.entrySet()) {
-				engine.put(entry.getKey(), entry.getValue());
-			}
+				for (Entry<String, Object> entry : engineAPI.entrySet()) {
+					engine.put(entry.getKey(), entry.getValue());
+				}
 
-			try {
-				return engine.eval(expression);
-			} catch (ScriptException e) {
-				AdvancedCorePlugin.getInstance().getLogger().warning(
-						"Error occoured while evaluating javascript, turn debug on to see stacktrace: " + e.toString());
-				AdvancedCorePlugin.getInstance().debug(e);
+				try {
+					return engine.eval(expression);
+				} catch (ScriptException e) {
+					AdvancedCorePlugin.getInstance().getLogger()
+							.warning("Error occoured while evaluating javascript, turn debug on to see stacktrace: "
+									+ e.toString());
+					AdvancedCorePlugin.getInstance().debug(e);
+				}
+			} else {
+				AdvancedCorePlugin.getInstance().debug("Failed to process javascript, engine == null");
+				return null;
 			}
 		}
 		return null;
