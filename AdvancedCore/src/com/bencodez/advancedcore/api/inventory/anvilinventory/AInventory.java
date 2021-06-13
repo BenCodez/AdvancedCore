f88af0bc94269d6ce7fcc2d8ae15667f3fe47604
BenCodez <benbergen12@gmail.com>
2021-06-13 13:41:51 -0400

    Fix javadoc

--- AdvancedCore/src/com/bencodez/advancedcore/api/inventory/anvilinventory/AInventory.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/inventory/anvilinventory/AInventory.java
@@ -1,8 +1,6 @@
 package com.bencodez.advancedcore.api.inventory.anvilinventory;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 import com.bencodez.advancedcore.AdvancedCorePlugin;
@@ -14,76 +12,7 @@ public class AInventory {
 
 	public interface AnvilClickEventHandler {
 
-		/**
-		 * On anvil click.
-		 *
-		 * @param event the event
-		 */
 		AnvilGUI.Response onAnvilClick(String value, Player player);
-	}
-
-	/**
-	 * The Enum AnvilSlot.
-	 */
-	public enum AnvilSlot {
-
-		/** The input left. */
-		INPUT_LEFT(0),
-
-		/** The input right. */
-		INPUT_RIGHT(1),
-
-		/** The output. */
-		OUTPUT(2);
-
-		/**
-		 * By slot.
-		 *
-		 * @param slot the slot
-		 * @return the anvil slot
-		 */
-		public static AnvilSlot bySlot(int slot) {
-			for (AnvilSlot anvilSlot : values()) {
-				if (anvilSlot.getSlot() == slot) {
-					return anvilSlot;
-				}
-			}
-
-			return null;
-		}
-
-		/** The slot. */
-		private int slot;
-
-		/**
-		 * Instantiates a new anvil slot.
-		 *
-		 * @param slot the slot
-		 */
-		private AnvilSlot(int slot) {
-			this.slot = slot;
-		}
-
-		/**
-		 * Gets the slot.
-		 *
-		 * @return the slot
-		 */
-		public int getSlot() {
-			return slot;
-		}
-	}
-
-	/**
-	 * Gets the version.
-	 *
-	 * @return the version
-	 */
-	public static String getVersion() {
-		Server server = Bukkit.getServer();
-		final String packageName = server.getClass().getPackage().getName();
-
-		return packageName.substring(packageName.lastIndexOf('.') + 1);
 	}
 
 	private void open(Player playerToOpen, AnvilClickEventHandler anvilClickEventHandler, String textToStart,
