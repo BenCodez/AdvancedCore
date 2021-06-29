2b51c8ac9947a08829a6f0c574adbef68d9c019f
BenCodez <benbergen12@gmail.com>
2021-06-28 21:13:47 -0400

    Improve hologramAPI

--- AdvancedCore/src/com/bencodez/advancedcore/api/hologram/Hologram.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/hologram/Hologram.java
@@ -1,5 +1,6 @@
 package com.bencodez.advancedcore.api.hologram;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -15,21 +16,93 @@ public class Hologram {
 	@Getter
 	private ArmorStand armorStand;
 
-	public Hologram(Location loc, String name) {
-		this.loc = loc;
+	private void createHologram(String name, boolean marker, boolean glowing) {
 		armorStand = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
 		armorStand.setVisible(false);
-		armorStand.setCustomNameVisible(true);
+		if (!name.isEmpty()) {
+			armorStand.setCustomNameVisible(true);
+		} else {
+			armorStand.setCustomNameVisible(false);
+		}
 		armorStand.setCustomName(StringParser.getInstance().colorize(name));
 		armorStand.setGravity(false);
 		armorStand.setAI(false);
-		armorStand.setMarker(true);
+		armorStand.setMarker(marker);
+		armorStand.setGlowing(glowing);
+	}
+
+	public Hologram(Location loc, String name) {
+		this.loc = loc;
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					createHologram(name, true, false);
+				}
+			});
+
+		} else {
+			createHologram(name, true, false);
+		}
+
+		AdvancedCorePlugin.getInstance().getHologramHandler().add(this);
+	}
+
+	public Hologram(Location loc, String name, boolean marker) {
+		this.loc = loc;
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					createHologram(name, marker, false);
+				}
+			});
+
+		} else {
+			createHologram(name, marker, false);
+		}
+		AdvancedCorePlugin.getInstance().getHologramHandler().add(this);
+	}
+	
+	public Hologram(Location loc, String name, boolean marker, boolean glowing) {
+		this.loc = loc;
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					createHologram(name, marker, glowing);
+				}
+			});
+
+		} else {
+			createHologram(name, marker, glowing);
+		}
 		AdvancedCorePlugin.getInstance().getHologramHandler().add(this);
 	}
 
 	public void kill() {
-		armorStand.setHealth(0);
-		armorStand.remove();
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					armorStand.setHealth(0);
+					armorStand.remove();
+				}
+			});
+		} else {
+			armorStand.setHealth(0);
+			armorStand.remove();
+		}
 		AdvancedCorePlugin.getInstance().getHologramHandler().remove(this, false);
+	}
+
+	public void glow(boolean value) {
+		if (armorStand != null) {
+			armorStand.setGlowing(value);
+		}
 	}
 }
