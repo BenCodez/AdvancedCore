02f3dd6eace52d0d159a3391d98f50375ddf427a
BenCodez <benbergen12@gmail.com>
2021-07-28 18:19:25 -0400

    Remove extra inventory check

--- AdvancedCore/src/com/bencodez/advancedcore/api/inventory/UpdatingBInventoryButton.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/inventory/UpdatingBInventoryButton.java
@@ -45,33 +45,29 @@ public abstract class UpdatingBInventoryButton extends BInventoryButton {
 	}
 
 	private void checkUpdate(Player p) {
-		if (getInv().isOpen(p)) {
-			final ItemStack item = onUpdate(p).toItemStack(p);
-			if (item != null) {
-				Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+		final ItemStack item = onUpdate(p).toItemStack(p);
+		if (item != null) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
 
-					@Override
-					public void run() {
-						try {
-							if (p != null && getInv().isOpen(p)) {
-								if (getFillSlots() != null && getFillSlots().size() > 0) {
-									for (Integer slot : getFillSlots()) {
-										p.getOpenInventory().getTopInventory().setItem(slot.intValue(), item);
-									}
-								} else {
-									p.getOpenInventory().getTopInventory().setItem(getSlot(), item);
+				@Override
+				public void run() {
+					try {
+						if (p != null && getInv().isOpen(p)) {
+							if (getFillSlots() != null && getFillSlots().size() > 0) {
+								for (Integer slot : getFillSlots()) {
+									p.getOpenInventory().getTopInventory().setItem(slot.intValue(), item);
 								}
+							} else {
+								p.getOpenInventory().getTopInventory().setItem(getSlot(), item);
 							}
-						} catch (Exception e) {
-							AdvancedCorePlugin.getInstance().debug(e);
-							cancel();
 						}
-
+					} catch (Exception e) {
+						AdvancedCorePlugin.getInstance().debug(e);
+						cancel();
 					}
-				});
-			} else {
-				cancel();
-			}
+
+				}
+			});
 		} else {
 			cancel();
 		}
