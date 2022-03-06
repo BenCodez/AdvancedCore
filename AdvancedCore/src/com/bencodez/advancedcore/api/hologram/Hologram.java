e6df11e060805968d4c3963e2f0df75d4d735e91
BenCodez <benbergen12@gmail.com>
2022-03-05 20:47:25 -0500

    Improve hologram API

--- AdvancedCore/src/com/bencodez/advancedcore/api/hologram/Hologram.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/hologram/Hologram.java
@@ -2,9 +2,11 @@ package com.bencodez.advancedcore.api.hologram;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import com.bencodez.advancedcore.AdvancedCorePlugin;
 import com.bencodez.advancedcore.api.messages.StringParser;
@@ -69,6 +71,23 @@ public class Hologram {
 		AdvancedCorePlugin.getInstance().getHologramHandler().add(this);
 	}
 
+	public Hologram(Location loc, String name, boolean marker, boolean glowing, NamespacedKey key, int value) {
+		this.loc = loc;
+		if (!Bukkit.isPrimaryThread()) {
+			Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					createHologram(name, marker, glowing, key, value);
+				}
+			});
+
+		} else {
+			createHologram(name, marker, glowing, key, value);
+		}
+		AdvancedCorePlugin.getInstance().getHologramHandler().add(this);
+	}
+
 	public PersistentDataContainer getPersistentDataHolder() {
 		return armorStand.getPersistentDataContainer();
 	}
@@ -90,6 +109,22 @@ public class Hologram {
 		armorStand.setAI(false);
 		armorStand.setMarker(marker);
 		armorStand.setGlowing(glowing);
+	}
+
+	private void createHologram(String name, boolean marker, boolean glowing, NamespacedKey key, int value) {
+		armorStand = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
+		armorStand.setVisible(false);
+		if (!name.isEmpty()) {
+			armorStand.setCustomNameVisible(true);
+		} else {
+			armorStand.setCustomNameVisible(false);
+		}
+		armorStand.setCustomName(StringParser.getInstance().colorize(name));
+		armorStand.setGravity(false);
+		armorStand.setAI(false);
+		armorStand.setMarker(marker);
+		armorStand.setGlowing(glowing);
+		armorStand.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, value);
 	}
 
 	public void glow(boolean value) {
