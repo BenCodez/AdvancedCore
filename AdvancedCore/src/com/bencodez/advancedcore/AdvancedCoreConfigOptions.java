c506fd7bcd9626af4942bbe4960dda0aad116feb
BenCodez <benbergen12@gmail.com>
2023-04-04 19:12:42 -0400

    Remove unused config option

--- AdvancedCore/src/com/bencodez/advancedcore/AdvancedCoreConfigOptions.java
+++ AdvancedCore/src/com/bencodez/advancedcore/AdvancedCoreConfigOptions.java
@@ -18,9 +18,6 @@ public class AdvancedCoreConfigOptions {
 
 	@Getter
 	@Setter
-	private boolean autoKillInvs = true;
-	@Getter
-	@Setter
 	private double clickSoundPitch = 1;
 	@Getter
 	@Setter
@@ -227,7 +224,6 @@ public class AdvancedCoreConfigOptions {
 			helpLine = configData.getString("Format.HelpLine", "&6%Command% - &6%HelpMessage%");
 			logDebugToFile = configData.getBoolean("LogDebugToFile", false);
 			sendScoreboards = configData.getBoolean("SendScoreboards", true);
-			autoKillInvs = configData.getBoolean("AutoKillInvs", true);
 			prevItem = configData.getConfigurationSection("Format.PrevItem");
 			nextItem = configData.getConfigurationSection("Format.NextItem");
 			formatRewardTimeFormat = configData.getString("Format.RewardTimeFormat", "EEE, d MMM yyyy HH:mm");
