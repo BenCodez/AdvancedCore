f0e7d33160f5b482222a546893b20373efbaf275
BenCodez <benbergen12@gmail.com>
2023-05-01 17:09:53 -0400

    Only allow updating items if user is cached

--- AdvancedCore/src/com/bencodez/advancedcore/api/inventory/UpdatingBInventoryButton.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/inventory/UpdatingBInventoryButton.java
@@ -46,37 +46,39 @@ public abstract class UpdatingBInventoryButton extends BInventoryButton {
 	}
 
 	private void checkUpdate(Player p) {
-		final ItemStack item = onUpdate(p).toItemStack(p);
-		if (item != null) {
-			if (AdvancedCorePlugin.getInstance().isEnabled()) {
-				Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
+		if (AdvancedCorePlugin.getInstance().getUserManager().getDataManager().isCached(p.getUniqueId())) {
+			final ItemStack item = onUpdate(p).toItemStack(p);
+			if (item != null) {
+				if (AdvancedCorePlugin.getInstance().isEnabled()) {
+					Bukkit.getScheduler().runTask(AdvancedCorePlugin.getInstance(), new Runnable() {
 
-					@Override
-					public void run() {
-						try {
-							if (p != null && getInv().isOpen(p)) {
-								if (getFillSlots() != null && getFillSlots().size() > 0) {
-									for (Integer slot : getFillSlots()) {
-										p.getOpenInventory().getTopInventory().setItem(slot.intValue(), item);
+						@Override
+						public void run() {
+							try {
+								if (p != null && getInv().isOpen(p)) {
+									if (getFillSlots() != null && getFillSlots().size() > 0) {
+										for (Integer slot : getFillSlots()) {
+											p.getOpenInventory().getTopInventory().setItem(slot.intValue(), item);
+										}
+									} else {
+										p.getOpenInventory().getTopInventory().setItem(getSlot(), item);
 									}
 								} else {
-									p.getOpenInventory().getTopInventory().setItem(getSlot(), item);
+									cancel();
 								}
-							} else {
+							} catch (Exception e) {
+								AdvancedCorePlugin.getInstance().debug(e);
 								cancel();
 							}
-						} catch (Exception e) {
-							AdvancedCorePlugin.getInstance().debug(e);
-							cancel();
-						}
 
-					}
-				});
+						}
+					});
+				} else {
+					cancel();
+				}
 			} else {
 				cancel();
 			}
-		} else {
-			cancel();
 		}
 	}
 
