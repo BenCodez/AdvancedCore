3c7c426e77627664408bfe75817e49d07d31d2d8
BenCodez <benbergen12@gmail.com>
2023-12-15 19:20:30 -0500

    Fix ItemsAdder handle issue

--- AdvancedCore/src/com/bencodez/advancedcore/api/item/ItemsAdderHandle.java
+++ AdvancedCore/src/com/bencodez/advancedcore/api/item/ItemsAdderHandle.java
@@ -10,7 +10,7 @@ public class ItemsAdderHandle {
 	}
 
 	public ItemStack getItem(String item) {
-		CustomStack stack = CustomStack.getInstance("your_item");
+		CustomStack stack = CustomStack.getInstance(item);
 		if (stack != null) {
 			return stack.getItemStack();
 		} else {
