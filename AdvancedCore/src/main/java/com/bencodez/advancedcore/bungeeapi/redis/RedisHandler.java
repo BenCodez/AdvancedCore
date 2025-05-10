e21f665811e6ec1519340ca4414bb1145734b38d
BenCodez <benbergen12@gmail.com>
2025-05-10 07:32:18 -0600

    Split RedisHandler into publish and subscribe pools.
    - Simplify message joining in sendMessage method.
    - Update loadListener to use subscribe pool.
    - Adjust close method to handle both pools.
    - Replace outdated sound in ExampleAdvanced.yml.
    - Add comment clarifying Minecraft sound naming.

--- AdvancedCore/src/main/java/com/bencodez/advancedcore/bungeeapi/redis/RedisHandler.java
+++ AdvancedCore/src/main/java/com/bencodez/advancedcore/bungeeapi/redis/RedisHandler.java
@@ -4,42 +4,39 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public abstract class RedisHandler {
-	JedisPool jedisPool;
+    private final JedisPool publishPool;
+    private final JedisPool subscribePool;
 
-	public RedisHandler(String host, int port, String username, String password) {
-		if (username.isEmpty() && password.isEmpty()) {
-			jedisPool = new JedisPool(host, port);
-		} else {
-			jedisPool = new JedisPool(host, port, username, password);
-		}
-	}
+    public RedisHandler(String host, int port, String username, String password) {
+        if (username.isEmpty() && password.isEmpty()) {
+            publishPool   = new JedisPool(host, port);
+            subscribePool = new JedisPool(host, port);
+        } else {
+            publishPool   = new JedisPool(host, port, username, password);
+            subscribePool = new JedisPool(host, port, username, password);
+        }
+    }
 
-	public void close() {
-		jedisPool.close();
-	}
+    public void close() {
+        publishPool.close();
+        subscribePool.close();
+    }
 
-	public abstract void debug(String message);
+    public abstract void debug(String message);
 
-	public void loadListener(RedisListener listener) {
-		try (Jedis jedis = jedisPool.getResource()) {
-			jedis.subscribe(listener, listener.getChannel());
-		}
-	}
+    public void loadListener(RedisListener listener) {
+        try (Jedis jedis = subscribePool.getResource()) {
+            jedis.subscribe(listener, listener.getChannel());
+        }
+    }
 
-	protected abstract void onMessage(String channel, String[] message);
+    protected abstract void onMessage(String channel, String[] message);
 
-	public void sendMessage(String channel, String... message) {
-		String str = "";
-		for (int i = 0; i < message.length; i++) {
-			str += message[i];
-			if (i < message.length - 1) {
-				str += ":";
-			}
-		}
-		try (Jedis jedis = jedisPool.getResource()) {
-			debug("Redis Send: " + channel + ", " + str);
-			jedis.publish(channel, str);
-		}
-	}
-
+    public void sendMessage(String channel, String... message) {
+        String str = String.join(":", message);
+        try (Jedis jedis = publishPool.getResource()) {
+            debug("Redis Send: " + channel + ", " + str);
+            jedis.publish(channel, str);
+        }
+    }
 }
