5457bc56b53f2c901f4778c533db66234286cc4d
Ben <benbergen12@gmail.com>
2026-08-23 10:24:27 -0600

    Preserve built-in reward registration order

--- AdvancedCore/src/main/java/com/bencodez/advancedcore/rewards/builtin/RewardCommands.java
+++ AdvancedCore/src/main/java/com/bencodez/advancedcore/rewards/builtin/RewardCommands.java
@@ -3,7 +3,6 @@ package com.bencodez.advancedcore.rewards.builtin;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -12,7 +11,6 @@ import org.bukkit.entity.Player;
 import com.bencodez.advancedcore.AdvancedCorePlugin;
 import com.bencodez.advancedcore.api.inventory.editgui.EditGUIButton;
 import com.bencodez.advancedcore.api.inventory.editgui.valuetypes.EditGUIValueList;
-import com.bencodez.advancedcore.api.inventory.editgui.valuetypes.EditGUIValueString;
 import com.bencodez.advancedcore.api.item.ItemBuilder;
 import com.bencodez.advancedcore.api.misc.MiscUtils;
 import com.bencodez.advancedcore.api.rewards.Reward;
@@ -20,64 +18,17 @@ import com.bencodez.advancedcore.api.rewards.RewardEditData;
 import com.bencodez.advancedcore.api.rewards.RewardHandler;
 import com.bencodez.advancedcore.api.rewards.injected.RewardInject;
 import com.bencodez.advancedcore.api.rewards.injected.RewardInjectConfigurationSection;
-import com.bencodez.advancedcore.api.rewards.injected.RewardInjectString;
 import com.bencodez.advancedcore.api.rewards.injected.RewardInjectStringList;
 import com.bencodez.advancedcore.api.rewards.injected.RewardInjectValidator;
 import com.bencodez.advancedcore.api.user.AdvancedCoreUser;
 
+/** Built-in plural Commands reward forms. */
 public final class RewardCommands {
 
     private RewardCommands() {
     }
 
     public static void register(RewardHandler handler, AdvancedCorePlugin plugin) {
-        handler.getInjectedRewards().add(new RewardInjectConfigurationSection("NumberCommand") {
-            @Override
-            public String onRewardRequested(Reward reward, AdvancedCoreUser user, ConfigurationSection section,
-                    HashMap<String, String> placeholders) {
-                int min = section.getInt("Min", 0);
-                int max = section.getInt("Max", 100);
-                int number = ThreadLocalRandom.current().nextInt(min, max + 1);
-                String command = section.getString("Command", "").replace("%number%", String.valueOf(number));
-                MiscUtils.getInstance().executeConsoleCommands(user.getPlayerName(), command, placeholders);
-                return String.valueOf(number);
-            }
-        }.asPlaceholder("Number").priority(100).validator(new RewardInjectValidator() {
-            @Override
-            public void onValidate(Reward reward, RewardInject inject, ConfigurationSection data) {
-                if (!data.isInt("NumberCommand.Min") || !data.isInt("NumberCommand.Max")
-                        || !data.isString("NumberCommand.Command")) {
-                    warning(reward, inject, "NumberCommand requires Min, Max, and Command to be set");
-                    return;
-                }
-                int min = data.getInt("NumberCommand.Min");
-                int max = data.getInt("NumberCommand.Max");
-                if (min > max) {
-                    warning(reward, inject, "NumberCommand Min can not be greater than Max");
-                }
-                if (min == max) {
-                    warning(reward, inject, "NumberCommand Min and Max are the same, random range is unnecessary");
-                }
-            }
-        }));
-
-        handler.getInjectedRewards().add(new RewardInjectString("Command") {
-            @Override
-            public String onRewardRequest(Reward reward, AdvancedCoreUser user, String value,
-                    HashMap<String, String> placeholders) {
-                MiscUtils.getInstance().executeConsoleCommands(user.getPlayerName(), value, placeholders);
-                return null;
-            }
-        }.addEditButton(new EditGUIButton(new ItemBuilder("COMMAND_BLOCK"), new EditGUIValueString("Command", null) {
-            @Override
-            public void setValue(Player player, String value) {
-                RewardEditData reward = (RewardEditData) getInv().getData("Reward");
-                reward.setValue(getKey(), value);
-                plugin.reloadAdvancedCore(false);
-                reward.reOpenEditGUI(player);
-            }
-        }.addLore("Execute single console command"))).validator(commandValidator()));
-
         handler.getInjectedRewards().add(new RewardInjectStringList("Commands") {
             @Override
             public String onRewardRequest(Reward reward, AdvancedCoreUser user, ArrayList<String> list,
@@ -146,47 +97,5 @@ public final class RewardCommands {
                         reward.reOpenEditGUI(player);
                     }
                 }.addLore("Execute commands as player"))));
-
-        handler.getInjectedRewards().add(new RewardInjectStringList("RandomCommand") {
-            @Override
-            public String onRewardRequest(Reward reward, AdvancedCoreUser user, ArrayList<String> list,
-                    HashMap<String, String> placeholders) {
-                if (!list.isEmpty()) {
-                    MiscUtils.getInstance().executeConsoleCommands(user.getPlayerName(),
-                            list.get(ThreadLocalRandom.current().nextInt(list.size())), placeholders);
-                }
-                return null;
-            }
-        }.addEditButton(new EditGUIButton(new ItemBuilder(Material.PAPER), new EditGUIValueList("RandomCommand", null) {
-            @Override
-            public void setValue(Player player, ArrayList<String> value) {
-                RewardEditData reward = (RewardEditData) getInv().getData("Reward");
-                reward.setValue(getKey(), value);
-                plugin.reloadAdvancedCore(false);
-                reward.reOpenEditGUI(player);
-            }
-        }.addLore("Execute random command"))).validator(new RewardInjectValidator() {
-            @Override
-            public void onValidate(Reward reward, RewardInject inject, ConfigurationSection data) {
-                List<String> list = data.getStringList(inject.getPath());
-                if (list.isEmpty()) {
-                    warning(reward, inject, "No rewards listed for random reward");
-                } else if (list.size() == 1) {
-                    warning(reward, inject, "Only one reward listed for random reward");
-                }
-            }
-        }));
-    }
-
-    private static RewardInjectValidator commandValidator() {
-        return new RewardInjectValidator() {
-            @Override
-            public void onValidate(Reward reward, RewardInject inject, ConfigurationSection data) {
-                String command = data.getString(inject.getPath());
-                if (command != null && command.startsWith("/")) {
-                    warning(reward, inject, "Can't start command with /");
-                }
-            }
-        };
     }
 }
